f14ea5d31af2dfae24f80b62136c0d69bf6a5fe3
Jhan Bayer <jhanbayer@gmail.com>
2022-06-28 20:09:11 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>HOla mundo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js, jsx}", "./*.{html,js, jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
